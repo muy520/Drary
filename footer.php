0a412120c20bbd0f1f397931870b92c5d645c4b3
牧羊 <42006467+muy520@users.noreply.github.com>
2019-09-14 21:58:18 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -21,6 +21,7 @@ function myFunction(){document.getElementById("myDropdown").classList.toggle("sh
 <script type="text/javascript" src="<?php $this->options->themeUrl('js/pinglun.js'); ?>"></script>
 <script type="text/javascript" src="<?php $this->options->themeUrl('js/jquery.pjax.js'); ?>"></script>
 <script type="text/javascript">$(document).pjax('a', '#pjax-container')</script>
+<script type="text/javascript">window.onload=()=>{var eles=document.querySelectorAll('.index-text-img');var callback=(entries)=>{entries.forEach(item=>{if(item.intersectionRatio>0){var ele=item.target;var imgSrc=ele.getAttribute('data-src');if(imgSrc){var img=new Image();img.addEventListener('load',function(){ele.src=imgSrc;},false);ele.src=imgSrc;ele.removeAttribute('data-src');}}})};var observer=new IntersectionObserver(callback);eles.forEach(item=>{observer.observe(item);})}</script>
 </body>
 </html>
 <?php if ($this->options->htmlCompress == 'able'): $html_source = ob_get_contents(); ob_clean(); print compressHtml($html_source); ob_end_flush(); endif; ?>
