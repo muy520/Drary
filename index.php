e263b6e7b169348ec1f39a65664fcedc2422136f
牧羊 <42006467+muy520@users.noreply.github.com>
2019-10-02 21:55:07 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,49 +15,26 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 <div class="index">
 	<div>
-	<?php if ($this->options->indexImg == 'able'): ?>
 	<?php while($this->next()): ?>
 		<article class="index-post-article">
-			<div class="index-post-img">
-		    		<div class="index-meta-img">
-					<time class="date-img"><?php $this->date('d'); ?></time>
-					<time class="week-img"><?php $this->date('D'); ?></time>
+			<div class="index-post">
+		    		<div class="index-meta">
+					<time class="date"><?php $this->date('d'); ?></time>
+					<time class="week"><?php $this->date('D'); ?></time>
 					<?php if (array_key_exists('weather',unserialize($this->___fields()))): ?><?php _e('<img class="icon-weather" src="/usr/themes/Diary/img/weather/') ?><?php $this->fields->weather(); ?><?php _e('.png" alt="') ?><?php $this->fields->weather(); ?><?php _e('" />') ?><?php else: ?><?php _e('<img class="icon-weather" alt=“sunny” src="/usr/themes/Diary/img/weather/sunny.png" />') ?><?php endif; ?>
 				</div>
 				<a href="<?php $this->permalink() ?>">
 					<?php if (array_key_exists('img',unserialize($this->___fields()))): ?><?php _e('<img class="index-text-img" src="./usr/themes/Diary/img/loading.gif" alt="background" data-src="') ?><?php $this->fields->img(); ?><?php _e('">') ?><?php else: ?><?php preg_match_all("/\<img.*?src\=(\'|\")(.*?)(\'|\")[^>]*>/i", $this->content, $matches);$imgCount = count($matches[0]);if($imgCount >= 1){$img = $matches[2][0];_e('<img class="index-text-img" alt="background" src="./usr/themes/Diary/img/loading.gif" data-src="'.$img.'">');}else {_e('<img class="index-text-img" alt="background" src="./usr/themes/Diary/img/loading.gif" data-src="/usr/themes/Diary/img/'.rand(1,10).'.png">');} ?><?php endif; ?>
 		        	<div class="text-img">
-	    				<p class="index-content-img"><?php $this->excerpt(50, '...');?></p>
+	    				<p class="index-content"><?php $this->excerpt(50, '...');?></p>
 		        	</div>
 	   			</a>
 		    	</div>
-	        <div class="title-img">
-	        	<h2 class="index-title-img"><?php $this->title() ?></h2>
+	        <div class="title">
+	        	<h2><?php $this->title() ?></h2>
 		</div>
 		</article>
 		<?php endwhile; ?>
-		<?php else: ?>
-		<?php while($this->next()): ?>
-	    	<a href="<?php $this->permalink() ?>">
-	    		<article class="index-post">
-	        		<div class="index-meta">
-					<time class="date"><?php $this->date('d'); ?></time>
-					<time class="week"><?php $this->date('D'); ?></time>
-				</div>
-				<div class="index-text">
-					<time><?php $this->date('H:i'); ?></time>
-					<h2 class="index-title"><?php $this->title() ?></h2>
-	    			<p class="index-content"><?php $this->excerpt(100, '...');?></p>
-		        	</div>
-	        		<a href="<?php $this->permalink() ?>#comments">
-	        			<div class="index-comment">
-	            			<?php $this->commentsNum('', '1', '%d'); ?>
-	            			</div>
-        			</a>
-	    		</article>
-	    	</a>
-		<?php endwhile; ?>
-		<?php endif; ?>
 		<div class="page-navigator">
 			<?php $this->pageLink('Prev '); ?>
 			<span class="page-number"><?php if($this->_currentPage>0) _e('Page '.$this->_currentPage.' of ')?><?php _e(ceil($this->getTotal() / $this->parameter->pageSize)) ?></span>
